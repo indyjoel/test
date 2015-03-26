3bca03c1cc45c57d2862e1cfa083daa3103d0fa8
indyjoel <indyjcl@IndyJCLs-MacBook-Pro.local>
2015-03-26 20:53:58 +1100

    commit

---
+++ Week 4/longForm/tabbedTest/tabbedTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tabbedTest
+//
+//  Created by IndyJCL on 25/03/2015.
+//  Copyright (c) 2015 JCL. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Week 4/longForm/tabbedTest/tabbedTest/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tabbedTest
+//
+//  Created by IndyJCL on 25/03/2015.
+//  Copyright (c) 2015 JCL. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
